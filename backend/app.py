375bc995747b0aef63af7676f1eac1fe2f09671e
RipNasty <det625@gmail.com>
2023-10-28 16:43:05 -0400

    made sql db

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://davidthomas@localhost/dndeasy_development'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
